5cf4b7b6d903cf2629ae3bef5b8987347828c3eb
Eli <elibo@live.com>
2016-01-22 14:58:48 +0100

    segunda

---
+++ Assets/Scripts/Bubbles.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bubbles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().sortingLayerName = "bubbles";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/fish.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fish : MonoBehaviour
+{
+
+    private float speed = 1;
+  
+   
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+
+    
+     
+
+    }
+
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+
+        speed *= -Random.Range(0.5f, 2.5f);
+        transform.Rotate(0, 180, 0);
+        transform.position += new Vector3(speed * Time.deltaTime, Random.value, 0);
+    }
+}
